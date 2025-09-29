e3a00b1920b5cc5ac58e4ec4f285d066d886e434
Viki Talim <talimdzioska53@gmail.com>
2025-09-29 20:44:07 +0200

    Update repo

---
+++ first_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "==Prva skripta!=="
+echo "==Momentalen direktorium: =="
+pwd
+
+mkdir demo_folder
+echo "Kreiravme folder so ime: demo_folder"
+
+echo "Hello Semos!" > ./demo_folder/hello.txt
+echo "Napravivme fajl vo demo_folderot"
+
+echo "Momentalen user:"
+whoami
+
+NAME="Viktorija"
+echo "Zdravo $NAME"
+
+echo "Finished script"
